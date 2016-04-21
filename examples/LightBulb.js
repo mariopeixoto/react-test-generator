cb4ec6b9c5b36d1ed75b8a634ac79ef20796150f
Mario Peixoto <mario.peixoto@gmail.com>
2016-04-20 20:39:58 -0500

    Adding declaration to example

--- examples/LightBulb.js
+++ examples/LightBulb.js
@@ -2,7 +2,8 @@ import React from 'react';
 
 class LightBulb extends React.Component {
   render() {
-    if (this.props.isOn) {
+    const { isOn } = this.props;
+    if (isOn) {
       return <div>Light is on</div>;
     } else {
       return <div>Light is off</div>;
