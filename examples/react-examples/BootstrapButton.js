23da04a40a6abc8021ea7158ef9464ffc995f4f6
Mario Peixoto <mario.peixoto@gmail.com>
2016-04-22 19:32:56 -0500

    adding unsupported component

---
+++ examples/react-examples/BootstrapButton.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var React = require('react');
+
+var BootstrapButton = React.createClass({
+  render: function() {
+    return (
+      <a {...this.props}
+        href="javascript:;"
+        role="button"
+        className={(this.props.className || '') + ' btn'} />
+    );
+  }
+});
+
+module.exports = BootstrapButton;
